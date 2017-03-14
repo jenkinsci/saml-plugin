1f891e7ecf18a1f330b8e77d68637cae306c91d5
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2017-03-14 23:30:10 +0100

    adds principal of the user to a log message

--- src/main/java/org/jenkinsci/plugins/saml/SamlSecurityRealm.java
+++ src/main/java/org/jenkinsci/plugins/saml/SamlSecurityRealm.java
@@ -39,6 +39,7 @@ import org.pac4j.saml.client.Saml2Client;
 import org.pac4j.saml.credentials.Saml2Credentials;
 import org.pac4j.saml.profile.Saml2Profile;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -295,8 +296,8 @@ public class SamlSecurityRealm extends SecurityRealm {
    * it will be done when we upgrade the library.
    */
   @Override
-  protected String getPostLogOutUrl(StaplerRequest req, Authentication auth) {
-    LOG.log(Level.FINE,"Doing Logout");
+  protected String getPostLogOutUrl(StaplerRequest req,@Nonnull Authentication auth) {
+    LOG.log(Level.FINE,"Doing Logout {}",auth.getPrincipal());
     // if we just redirect to the root and anonymous does not have Overall read then we will start a login all over again.
     // we are actually anonymous here as the security context has been cleared
     if (Jenkins.getActiveInstance().hasPermission(Jenkins.READ)) {
